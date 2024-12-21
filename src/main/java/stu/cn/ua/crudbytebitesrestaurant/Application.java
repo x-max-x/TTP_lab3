07dfe2c12577a2b83ad6e0f42e9e4b16934d3607
x-max-x <68254545+x-max-x@users.noreply.github.com>
2024-12-21 23:26:31 +0200

    Add files via upload

---
+++ src/main/java/stu/cn/ua/crudbytebitesrestaurant/Application.java
@@ -0,0 +1,16 @@
+package stu.cn.ua.crudbytebitesrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
